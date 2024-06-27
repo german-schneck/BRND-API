1c3629b951e885bee9faf449759e24c1051e2438
German D. Schneck <german.schneck@gmail.com>
2024-06-27 08:19:01 -0300

    ft(master): added brand controllers

--- src/core/brand/brand.controller.ts
+++ src/core/brand/brand.controller.ts
@@ -1,6 +1,16 @@
 // Dependencies
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 
 // Services
 import { BrandService } from './services';
@@ -8,13 +18,77 @@ import { BrandService } from './services';
 // Models
 import { Brand } from '../../models';
 
+// Utils
+import { HttpStatus, hasError, hasResponse } from '../../utils';
+
 @ApiTags('brand-service')
 @Controller('brand-service')
 export class BrandController {
   constructor(private readonly brandService: BrandService) {}
 
+  /**
+   * Retrieves a brand by its ID.
+   *
+   * @param {Brand['id']} id - The ID of the brand to retrieve.
+   * @returns {Promise<Brand | undefined>} The brand entity or undefined if not found.
+   */
   @Get('/brand/:id')
   getBrandById(@Param('id') id: Brand['id']) {
     return this.brandService.getById(id);
+  }
+
+  @Put('/vote')
+  async voteBrands(@Body() { ids }: { ids: string[] }, @Res() res: Response) {
+    if (!Array.isArray(ids) || ids.length !== 3) {
+      return hasError(
+        res,
+        HttpStatus.BAD_REQUEST,
+        'voteBrands',
+        'Invalid input: exactly 3 brand IDs are required.',
+      );
+    }
+
+    try {
+      // Assuming there's a method in brandService to handle the voting logic
+      //await this.brandService.voteForBrands(ids);
+      return hasResponse(res, { message: 'Vote recorded successfully.' });
+    } catch (error) {
+      return hasError(
+        res,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        'voteBrands',
+        'An unexpected error occurred.',
+      );
+    }
+  }
+  /**
+   * Retrieves all brands with pagination.
+   *
+   * @param {string} search - The search query to filter brands.
+   * @param {number} pageId - The ID of the page to retrieve.
+   * @param {number} limit - The number of brands to retrieve per page.
+   * @param {Response} res - The response object.
+   * @returns {Promise<Response>} A response object containing the page ID, total count of brands, and an array of brand objects.
+   */
+  @Get('/all')
+  async getAllBrands(
+    @Query('search') search: string,
+    @Query('pageId') pageId: number,
+    @Query('limit') limit: number,
+    @Res() res: Response,
+  ) {
+    const [brands, count] = await this.brandService.getAll(
+      ['id', 'name', 'url', 'imageUrl'],
+      [],
+      search,
+      pageId,
+      limit,
+    );
+
+    return hasResponse(res, {
+      pageId,
+      count,
+      brands,
+    });
   }
 }
--- src/core/brand/services/brand.service.ts
+++ src/core/brand/services/brand.service.ts
@@ -1,7 +1,7 @@
 // Dependencies
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 
 // Models
 import { Brand } from '../../../models';
@@ -33,6 +33,43 @@ export class BrandService {
       where: {
         id,
       },
+      ...(relations.length > 0 && {
+        relations,
+      }),
+    });
+  }
+
+  /**
+   * Retrieves all brands with pagination.
+   *
+   * @param {number} [pageId=1] - The page number to retrieve.
+   * @param {number} [limit=15] - The number of brands to retrieve per page.
+   * @returns {Promise<[Brand[], number]>} A promise that resolves to an array containing the list of brands and the total count.
+   */
+  async getAll(
+    select: (keyof Brand)[] = [],
+    relations: (keyof Brand)[] = [],
+    searchName: string = '',
+    pageId: number = 1,
+    limit: number = 15,
+  ): Promise<[Brand[], number]> {
+    const hasSearch = searchName !== '';
+
+    const skip = (pageId - 1) * limit;
+    return this.brandRepository.findAndCount({
+      ...(select.length > 0 && {
+        select,
+      }),
+
+      skip,
+      take: limit,
+
+      ...(hasSearch && {
+        where: {
+          name: Like(`%${searchName}%`),
+        },
+      }),
+
       ...(relations.length > 0 && {
         relations,
       }),
